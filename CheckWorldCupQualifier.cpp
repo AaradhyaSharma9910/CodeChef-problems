e7c7d16f4d76b75688f231ce85a44c6cb2582cac
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-05-30 22:59:59 +0530

    added check world qualifer problem which is rated 203 on codechef

---
+++ CheckWorldCupQualifier.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+ //cout<<"Enter the total number of points that team has scored =\n";
+ cin>>n;
+
+ if(n>=12){
+     cout<<"yes";
+ }else{
+     cout<<"no";
+ }
+ return 0;
+}
