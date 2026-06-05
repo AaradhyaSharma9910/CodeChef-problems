d749dc6cb646fb2b60fa0eb45fe86c20761bd2a7
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-06-05 23:01:48 +0530

    added codechef problem which is rated 279

---
+++ problems rated 0-500/ReachOnTime.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    //int z = 30-n;
+    if(n>=30){
+        cout<<"yes"<<endl;
+    }else{
+        cout<<"no"<<endl;
+    }
+}
+}
