214ff2ec726c6d95ad180156d7857b1e850ec2ec
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-06-09 21:24:23 +0530

    added problem which is rated 281

---
+++ problems rated 0-500/WhoIsTaller!.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    int x;
+    cin>>x;
+    if(n>x){
+        cout<<"A"<<endl;
+    }else{
+        cout<<"B"<<endl;
+    }
+}
+return 0;
+}
