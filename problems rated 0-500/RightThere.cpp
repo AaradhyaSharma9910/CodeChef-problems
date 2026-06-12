34f80a95886b6d111edb6b5c1e8c51abded2c98c
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-06-12 22:57:48 +0530

    added problem which is rated 99

---
+++ problems rated 0-500/RightThere.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t!=0){
+    int a,b;
+    cin>>a>>b;
+    if(a<=b){
+        cout<<"yes"<<endl;
+    }else{
+        cout<<"no"<<endl;
+    }
+    t--;
+}
+return 0;
+}
