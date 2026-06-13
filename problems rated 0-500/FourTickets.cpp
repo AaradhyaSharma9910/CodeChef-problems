e16757efda5a376f7ea40c85640ca6c245ac45d6
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-06-13 22:05:12 +0530

    added problem which is rated 302

---
+++ problems rated 0-500/FourTickets.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int a;
+    cin>>a;
+    int x;
+    x=4*a;
+    if(x<=1000){
+        cout<<"yes"<<endl;
+    }else{
+        cout<<"no"<<endl;
+    }
+}
+}
