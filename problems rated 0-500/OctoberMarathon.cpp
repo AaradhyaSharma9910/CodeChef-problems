fa3f871307f58b33157528867da8810dd09f091b
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-06-15 21:47:04 +0530

    added probelm which is rated 319

---
+++ problems rated 0-500/OctoberMarathon.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n;
+    cin>>n;
+    if(n<3){
+        cout<<"GOLD"<<endl;
+    }else if(n<6){
+        cout<<"SILVER"<<endl;
+    }else{
+        cout<<"BRONZE"<<endl;
+    }
+}
+
