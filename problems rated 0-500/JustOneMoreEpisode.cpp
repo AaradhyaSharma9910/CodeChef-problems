74a7c6bbdc2777b1165846dbe2369e94a25d9e7a
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-06-16 14:32:10 +0530

    added problem which is rated 320

---
+++ problems rated 0-500/JustOneMoreEpisode.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;cin>>t;
+while(t--){
+    int a;
+    cin>>a;
+    if(a>24){
+        cout<<"yes"<<endl;
+    }else{
+        cout<<"No"<<endl;
+    }
+}
+}
