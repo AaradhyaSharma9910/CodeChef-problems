188e22f551bf013b19da0f948c0c34d5ebf6235a
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-06-18 23:05:58 +0530

    added problem which is rated 337

---
+++ problems rated 0-500/ChessTime.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;cin>>t;
+while(t--){
+    int a;cin>>a;
+    int k = a*60;
+    cout<<k/20<<endl;
+}
+}
