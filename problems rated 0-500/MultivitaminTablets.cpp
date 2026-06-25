9dcad3a5938b8a1024f1dd1927e5f281106aa09c
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-06-25 22:28:59 +0530

    added problem rated 376

---
+++ problems rated 0-500/MultivitaminTablets.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int a,b;
+    cin>>a>>b;
+    int y = a * 3;
+    if(y<=b){
+        cout<<"yes"<<endl;
+    }else{
+        cout<<"no"<<endl;
+    }
+}
+}
