bd69814a6a28384341823b8a5c5975ded98109d8
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-06-25 09:14:15 +0530

    added problem rated 357

---
+++ problems rated 0-500/GoodInvestmentOrNot.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int a,b;
+    cin>>a>>b;
+    int x = b*2;
+    if(a<x){
+        cout<<"no"<<endl;
+    }else{
+        cout<<"yes"<<endl;
+    }
+}
+}
