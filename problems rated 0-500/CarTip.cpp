6c0e94ea8e8322ad608e2e37afa187ba459ce598
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-06-25 09:18:17 +0530

    added problem rated 374

---
+++ problems rated 0-500/CarTip.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int a;
+    cin>>a;
+    if(a<300){
+        cout<<300*10<<endl;
+    }else{
+        cout<<a*10<<endl;
+    }
+    
+}
+}
