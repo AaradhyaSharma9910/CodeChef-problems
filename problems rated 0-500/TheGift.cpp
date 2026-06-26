387e4169a5cd6d14fbae4eee53cfc78daa60a381
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-06-26 09:43:43 +0530

    added problem rated 390

---
+++ problems rated 0-500/TheGift.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int a,b,c;
+cin>>a>>b>>c;
+if(a+c>=b){
+    cout<<"yes"<<endl;
+}else{
+    cout<<"no"<<endl;
+}
+}
