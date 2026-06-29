3b68d1d3e9803ae46b9b789d231545eb27470af4
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-06-29 22:26:31 +0530

    added problem rated 414

---
+++ problems rated 0-500/ProfitIncrement.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int a,b;
+    cin>>a>>b;
+int k = a-b;    
+int z = a/10;
+int x = a+z;
+
+cout<<x-k<<endl;
+}
+}
