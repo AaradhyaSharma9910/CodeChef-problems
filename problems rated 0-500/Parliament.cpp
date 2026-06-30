add6640a2d9515d8a66e63842f8986f5a537c08b
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-06-30 23:25:25 +0530

    added problem rated 419

---
+++ problems rated 0-500/Parliament.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    double a,b;
+    cin>>a;
+    cin>>b;
+    if(a/2==b){
+        cout<<"yes"<<endl;
+    }else if(a/2<=b){
+        cout<<"yes"<<endl;
+        }else{
+        cout<<"no"<<endl;
+    }
+}
+}
