b929115f5ef5664102b3d6397af7086070e78676
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-07-05 23:49:09 +0530

    added problem rated 470

---
+++ problems rated 0-500/CourseRegistration.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if((a+c)<=b){
+        cout<<"yes"<<endl;
+    }else{
+        cout<<"no"<<endl;
+    }
+}
+}
