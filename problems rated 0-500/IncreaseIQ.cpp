b984661b366a717fb21e8112f26a3dec7b582c3d
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-07-06 17:42:03 +0530

    added problem rated 478

---
+++ problems rated 0-500/IncreaseIQ.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int a;
+cin>>a;
+int sum = a + 7;
+if(sum>170){
+    cout<<"yes"<<endl;
+}else{
+    cout<<"no"<<endl;
+}
+}
