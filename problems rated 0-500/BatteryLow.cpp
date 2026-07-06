83276a4ccb21e9ca5e62333ed56b4b2ab780da5c
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-07-06 17:43:13 +0530

    added problem rated 479

---
+++ problems rated 0-500/BatteryLow.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int a;
+    cin>>a;
+    if(a<=15){
+        cout<<"yes"<<endl;
+    }else{
+        cout<<"no"<<endl;
+    }
+}
+}
