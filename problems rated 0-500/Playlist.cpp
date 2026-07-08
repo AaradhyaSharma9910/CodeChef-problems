0281e2c64e85e8f6e164cc134672870651d63a50
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-07-08 10:32:50 +0530

    added problem rated 489

---
+++ problems rated 0-500/Playlist.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int a,b;
+    cin>>a>>b;
+    int result = a/(3*b);
+    cout<<result<<endl;
+}
+return 0;
+}
