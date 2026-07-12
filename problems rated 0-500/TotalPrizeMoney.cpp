6903f674afc0b1a44cae878b623c7b15ec074c66
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-07-12 13:42:48 +0530

    added problem rated 296

---
+++ problems rated 0-500/TotalPrizeMoney.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+int x = a*10;
+int y = b * 90;
+cout<< x+y<< endl;
+
+}}
