13f9bf303ec5e6585cc2a975360fee60767f40a8
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-07-15 22:14:21 +0530

    added problem rated 513

---
+++ problems rated 500-1000/JanmanshAndAssignments.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a;
+	    cin>>a;
+	    int x = a + 3;
+	    if(x<=10){
+	        cout<<"yes"<<endl;
+	    }else{
+	        cout<<"no"<<endl;
+	    }
+	}
+
+}
