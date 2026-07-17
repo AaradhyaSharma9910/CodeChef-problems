74998d66ee4ad84810be6a93e0cf16456c07586c
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-07-17 21:57:47 +0530

    added problem rated 539

---
+++ problems rated 500-1000/CREDCoins.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int a,b;
+    cin>>a>>b;
+    int x = a * b;
+    //int cnt;
+    cout<<x/100<<endl;
+    //lse cnt2++;
+    //cout<<
+}
+}
