1f806a354d27c8f74f5bc3528145332e59e7153d
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-07-18 22:47:02 +0530

    added problem rated 541

---
+++ problems rated 500-1000/WaterFilling.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a==1 && b==1 || b==1 && c==1 || a==1 && c==1) cout<<"not now"<<endl;
+    else cout<<"water filling time"<<endl;
+}
+}
