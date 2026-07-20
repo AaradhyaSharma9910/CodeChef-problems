0107eff62737216e11708706d7797acc0fd9ab47
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-07-20 13:33:53 +0530

    added problem rated 561

---
+++ problems rated 500-1000/ExpertSetter.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int a,b;
+    cin>>a>>b;
+    if(2*b>=a) cout<<"yes"<<endl;
+    else cout<<"no"<<endl;
+}
+}
