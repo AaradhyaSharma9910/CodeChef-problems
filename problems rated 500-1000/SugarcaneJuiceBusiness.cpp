2c2bd08d5838bc3226e75bf3592fa79696c4116e
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-07-21 19:05:46 +0530

    added problem rated 563

---
+++ problems rated 500-1000/SugarcaneJuiceBusiness.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int a;
+    cin>>a;
+    int x = 50 * a;
+    int y = 10 * a;
+    int z = 15 * a;
+    cout<<x-(2*y+z)<<endl;
+}
+}
