78ac84be72257c244a689f4c3d8437eb8c1ba6c4
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-07-21 19:06:24 +0530

    added problem rated 563

---
+++ problems rated 500-1000/CountTheNotebooks.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int a;
+    cin>>a;
+    int x = a * 100;
+    int y = x / 10;
+    cout<<y<<endl;
+}
+}
