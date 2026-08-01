33e79201911e7df97925a154fa4da7b1db78ac64
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-08-01 23:34:31 +0530

    added problem rated 610

---
+++ problems rated 500-1000/TestScore.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(c%b == 0 && c <= a * b) cout<<"yes"<<endl;
+    else cout <<"no"<<endl;
+}
+}
