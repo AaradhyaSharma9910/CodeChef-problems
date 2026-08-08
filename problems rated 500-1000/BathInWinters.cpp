fc3d7a7746c46de9903f32a56c02c444fa3f8abd
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-08-08 22:52:53 +0530

    added problem rated 643

---
+++ problems rated 500-1000/BathInWinters.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int a,b;
+    cin>>a>>b;
+    int x = b * 2;
+    int y = a / x;
+    cout<<y<<endl;
+}
+}
