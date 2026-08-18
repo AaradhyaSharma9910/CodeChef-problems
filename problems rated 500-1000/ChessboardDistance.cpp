6f165804854c90c37942acd722856ff4c3609d00
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-08-18 21:25:11 +0530

    added problem rated 690

---
+++ problems rated 500-1000/ChessboardDistance.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<max(abs(a-c),abs(b-d))<<endl;
+}
+}
