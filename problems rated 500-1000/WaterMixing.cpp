3f6f2231b5c29e2a6e15696f9f2cd32121f30394
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-08-21 23:00:40 +0530

    added problem rated 694

---
+++ problems rated 500-1000/WaterMixing.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    if(b >= a - d && b <= a + c) cout<<"yes"<<endl;
+    else cout<<"no"<<endl;
+}
+}
