628fc2259a6ea00b06bd79d6b6c5846f9a7b2497
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-09-12 22:19:51 +0530

    added problem rated 763

---
+++ problems rated 500-1000/DraculaEats.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+int n;
+cin>>n;
+int count = 0;
+for ( int i = 2; i <= n; i += 7){
+    count++;
+}
+cout<<count<<endl;
+}
+}
