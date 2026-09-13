e46e2da9d05d0622a6470e1231ff2cf9efeded5d
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-09-13 19:47:57 +0530

    added problem rated 769

---
+++ problems rated 500-1000/PossibleVictory.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int r,o,c;
+cin>>r>>o>>c;
+if(c+(20-o)*36>r){
+    cout<<"yes"<<endl;
+}else{
+    cout<<"no"<<endl;
+}
+}
