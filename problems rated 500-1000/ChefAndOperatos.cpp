2508802b70c32a9e48a50a9dbd99c6e7429fe724
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-09-14 22:57:49 +0530

    added problem rated 770

---
+++ problems rated 500-1000/ChefAndOperatos.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    long long a,b;
+    cin>>a>>b;
+    if(a<b) cout<<"<"<<endl;
+    else if (a>b) cout<<">"<<endl;
+    else cout<<"="<<endl;
+}
+}
